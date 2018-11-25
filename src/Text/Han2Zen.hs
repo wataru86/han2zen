e3d6456719b9d66bb1311c5a3ceb506a5b05d7df
wataru86 <is151086@cis.aichi-pu.ac.jp>
2018-11-25 18:00:27 +0900

    Add han2zenSafe

--- src/Text/Han2Zen.hs
+++ src/Text/Han2Zen.hs
@@ -1,10 +1,20 @@
 module Text.Han2Zen
     ( han2zen
+    , han2zenSafe
     ) where
 
 -- | テキスト内の半角カタカナを全角にに変える
+
+import Data.Maybe
+
 han2zen :: String -> String
-han2zen x = map hanKanaToZenKana (chars x)
+han2zen x = map hanKanaToZenKana' (chars x)
+
+hanKanaToZenKana' :: String -> Char
+hanKanaToZenKana' x = fromMaybe (error $ "Syntax Error: " ++ x) $ hanKanaToZenKana x
+
+han2zenSafe :: String -> Maybe String
+han2zenSafe x = mapM hanKanaToZenKana (chars x)
 
 -- | 半角の濁点の有無で文字ごとに分ける
 chars :: String -> [String]
@@ -12,7 +22,7 @@ chars [] = []
 chars [x] = [[x]]
 chars txt
   | isDakuten (txt !! 1) = take 2 txt : chars (drop 2 txt)
-  | otherwise                 = take 1 txt : chars (drop 1 txt)
+  | otherwise            = take 1 txt : chars (drop 1 txt)
 
 -- | 濁点または半濁点を判別する
 isDakuten :: Char -> Bool
@@ -21,98 +31,98 @@ isDakuten 'ﾟ' = True
 isDakuten _   = False
 
 -- | 半角を全角文字に
-hanKanaToZenKana :: String -> Char
-hanKanaToZenKana "｡" = '。'
-hanKanaToZenKana "｢" = '「'
-hanKanaToZenKana "｣" = '」'
-hanKanaToZenKana "､" = '、'
-hanKanaToZenKana "･" = '・'
-hanKanaToZenKana "ｦ" = 'ヲ'
-hanKanaToZenKana "ｧ" = 'ァ'
-hanKanaToZenKana "ｨ" = 'ィ'
-hanKanaToZenKana "ｩ" = 'ゥ'
-hanKanaToZenKana "ｪ" = 'ェ'
-hanKanaToZenKana "ｫ" = 'ォ'
-hanKanaToZenKana "ｬ" = 'ャ'
-hanKanaToZenKana "ｭ" = 'ュ'
-hanKanaToZenKana "ｮ" = 'ョ'
-hanKanaToZenKana "ｯ" = 'ッ'
-hanKanaToZenKana "ｰ" = 'ー'
-hanKanaToZenKana "ｱ" = 'ア'
-hanKanaToZenKana "ｲ" = 'イ'
-hanKanaToZenKana "ｳ" = 'ウ'
-hanKanaToZenKana "ｴ" = 'エ'
-hanKanaToZenKana "ｵ" = 'オ'
-hanKanaToZenKana "ｶ" = 'カ'
-hanKanaToZenKana "ｷ" = 'キ'
-hanKanaToZenKana "ｸ" = 'ク'
-hanKanaToZenKana "ｹ" = 'ケ'
-hanKanaToZenKana "ｺ" = 'コ'
-hanKanaToZenKana "ｻ" = 'サ'
-hanKanaToZenKana "ｼ" = 'シ'
-hanKanaToZenKana "ｽ" = 'ス'
-hanKanaToZenKana "ｾ" = 'セ'
-hanKanaToZenKana "ｿ" = 'ソ'
-hanKanaToZenKana "ﾀ" = 'タ'
-hanKanaToZenKana "ﾁ" = 'チ'
-hanKanaToZenKana "ﾂ" = 'ツ'
-hanKanaToZenKana "ﾃ" = 'テ'
-hanKanaToZenKana "ﾄ" = 'ト'
-hanKanaToZenKana "ﾅ" = 'ナ'
-hanKanaToZenKana "ﾆ" = 'ニ'
-hanKanaToZenKana "ﾇ" = 'ヌ'
-hanKanaToZenKana "ﾈ" = 'ネ'
-hanKanaToZenKana "ﾉ" = 'ノ'
-hanKanaToZenKana "ﾊ" = 'ハ'
-hanKanaToZenKana "ﾋ" = 'ヒ'
-hanKanaToZenKana "ﾌ" = 'フ'
-hanKanaToZenKana "ﾍ" = 'ヘ'
-hanKanaToZenKana "ﾎ" = 'ホ'
-hanKanaToZenKana "ﾏ" = 'マ'
-hanKanaToZenKana "ﾐ" = 'ミ'
-hanKanaToZenKana "ﾑ" = 'ム'
-hanKanaToZenKana "ﾒ" = 'メ'
-hanKanaToZenKana "ﾓ" = 'モ'
-hanKanaToZenKana "ﾔ" = 'ヤ'
-hanKanaToZenKana "ﾕ" = 'ユ'
-hanKanaToZenKana "ﾖ" = 'ヨ'
-hanKanaToZenKana "ﾗ" = 'ラ'
-hanKanaToZenKana "ﾘ" = 'リ'
-hanKanaToZenKana "ﾙ" = 'ル'
-hanKanaToZenKana "ﾚ" = 'レ'
-hanKanaToZenKana "ﾛ" = 'ロ'
-hanKanaToZenKana "ﾜ" = 'ワ'
-hanKanaToZenKana "ﾝ" = 'ン'
-hanKanaToZenKana "ﾞ" = '゛' -- これいる？
-hanKanaToZenKana "ﾟ" = '゜' -- これいる？
-hanKanaToZenKana "ｶﾞ" = 'ガ'
-hanKanaToZenKana "ｷﾞ" = 'ギ'
-hanKanaToZenKana "ｸﾞ" = 'グ'
-hanKanaToZenKana "ｹﾞ" = 'ゲ'
-hanKanaToZenKana "ｺﾞ" = 'ゴ'
-hanKanaToZenKana "ｻﾞ" = 'ザ'
-hanKanaToZenKana "ｼﾞ" = 'ジ'
-hanKanaToZenKana "ｽﾞ" = 'ズ'
-hanKanaToZenKana "ｾﾞ" = 'ゼ'
-hanKanaToZenKana "ｿﾞ" = 'ゾ'
-hanKanaToZenKana "ﾀﾞ" = 'ダ'
-hanKanaToZenKana "ﾁﾞ" = 'ヂ'
-hanKanaToZenKana "ﾂﾞ" = 'ヅ'
-hanKanaToZenKana "ﾃﾞ" = 'デ'
-hanKanaToZenKana "ﾄﾞ" = 'ド'
-hanKanaToZenKana "ﾊﾞ" = 'バ'
-hanKanaToZenKana "ﾋﾞ" = 'ビ'
-hanKanaToZenKana "ﾌﾞ" = 'ブ'
-hanKanaToZenKana "ﾍﾞ" = 'ベ'
-hanKanaToZenKana "ﾎﾞ" = 'ボ'
-hanKanaToZenKana "ﾊﾟ" = 'パ'
-hanKanaToZenKana "ﾋﾟ" = 'ピ'
-hanKanaToZenKana "ﾌﾟ" = 'プ'
-hanKanaToZenKana "ﾍﾟ" = 'ペ'
-hanKanaToZenKana "ﾎﾟ" = 'ポ'
-hanKanaToZenKana "ｳﾞ" = 'ヴ'
-hanKanaToZenKana [c] = c
-hanKanaToZenKana xs = error ("syntax error:" ++ xs)
+hanKanaToZenKana :: String -> Maybe Char
+hanKanaToZenKana "｡" = Just '。'
+hanKanaToZenKana "｢" = Just '「'
+hanKanaToZenKana "｣" = Just '」'
+hanKanaToZenKana "､" = Just '、'
+hanKanaToZenKana "･" = Just '・'
+hanKanaToZenKana "ｦ" = Just 'ヲ'
+hanKanaToZenKana "ｧ" = Just 'ァ'
+hanKanaToZenKana "ｨ" = Just 'ィ'
+hanKanaToZenKana "ｩ" = Just 'ゥ'
+hanKanaToZenKana "ｪ" = Just 'ェ'
+hanKanaToZenKana "ｫ" = Just 'ォ'
+hanKanaToZenKana "ｬ" = Just 'ャ'
+hanKanaToZenKana "ｭ" = Just 'ュ'
+hanKanaToZenKana "ｮ" = Just 'ョ'
+hanKanaToZenKana "ｯ" = Just 'ッ'
+hanKanaToZenKana "ｰ" = Just 'ー'
+hanKanaToZenKana "ｱ" = Just 'ア'
+hanKanaToZenKana "ｲ" = Just 'イ'
+hanKanaToZenKana "ｳ" = Just 'ウ'
+hanKanaToZenKana "ｴ" = Just 'エ'
+hanKanaToZenKana "ｵ" = Just 'オ'
+hanKanaToZenKana "ｶ" = Just 'カ'
+hanKanaToZenKana "ｷ" = Just 'キ'
+hanKanaToZenKana "ｸ" = Just 'ク'
+hanKanaToZenKana "ｹ" = Just 'ケ'
+hanKanaToZenKana "ｺ" = Just 'コ'
+hanKanaToZenKana "ｻ" = Just 'サ'
+hanKanaToZenKana "ｼ" = Just 'シ'
+hanKanaToZenKana "ｽ" = Just 'ス'
+hanKanaToZenKana "ｾ" = Just 'セ'
+hanKanaToZenKana "ｿ" = Just 'ソ'
+hanKanaToZenKana "ﾀ" = Just 'タ'
+hanKanaToZenKana "ﾁ" = Just 'チ'
+hanKanaToZenKana "ﾂ" = Just 'ツ'
+hanKanaToZenKana "ﾃ" = Just 'テ'
+hanKanaToZenKana "ﾄ" = Just 'ト'
+hanKanaToZenKana "ﾅ" = Just 'ナ'
+hanKanaToZenKana "ﾆ" = Just 'ニ'
+hanKanaToZenKana "ﾇ" = Just 'ヌ'
+hanKanaToZenKana "ﾈ" = Just 'ネ'
+hanKanaToZenKana "ﾉ" = Just 'ノ'
+hanKanaToZenKana "ﾊ" = Just 'ハ'
+hanKanaToZenKana "ﾋ" = Just 'ヒ'
+hanKanaToZenKana "ﾌ" = Just 'フ'
+hanKanaToZenKana "ﾍ" = Just 'ヘ'
+hanKanaToZenKana "ﾎ" = Just 'ホ'
+hanKanaToZenKana "ﾏ" = Just 'マ'
+hanKanaToZenKana "ﾐ" = Just 'ミ'
+hanKanaToZenKana "ﾑ" = Just 'ム'
+hanKanaToZenKana "ﾒ" = Just 'メ'
+hanKanaToZenKana "ﾓ" = Just 'モ'
+hanKanaToZenKana "ﾔ" = Just 'ヤ'
+hanKanaToZenKana "ﾕ" = Just 'ユ'
+hanKanaToZenKana "ﾖ" = Just 'ヨ'
+hanKanaToZenKana "ﾗ" = Just 'ラ'
+hanKanaToZenKana "ﾘ" = Just 'リ'
+hanKanaToZenKana "ﾙ" = Just 'ル'
+hanKanaToZenKana "ﾚ" = Just 'レ'
+hanKanaToZenKana "ﾛ" = Just 'ロ'
+hanKanaToZenKana "ﾜ" = Just 'ワ'
+hanKanaToZenKana "ﾝ" = Just 'ン'
+hanKanaToZenKana "ﾞ" = Just '゛' -- これいる？
+hanKanaToZenKana "ﾟ" = Just '゜' -- これいる？
+hanKanaToZenKana "ｶﾞ" = Just 'ガ'
+hanKanaToZenKana "ｷﾞ" = Just 'ギ'
+hanKanaToZenKana "ｸﾞ" = Just 'グ'
+hanKanaToZenKana "ｹﾞ" = Just 'ゲ'
+hanKanaToZenKana "ｺﾞ" = Just 'ゴ'
+hanKanaToZenKana "ｻﾞ" = Just 'ザ'
+hanKanaToZenKana "ｼﾞ" = Just 'ジ'
+hanKanaToZenKana "ｽﾞ" = Just 'ズ'
+hanKanaToZenKana "ｾﾞ" = Just 'ゼ'
+hanKanaToZenKana "ｿﾞ" = Just 'ゾ'
+hanKanaToZenKana "ﾀﾞ" = Just 'ダ'
+hanKanaToZenKana "ﾁﾞ" = Just 'ヂ'
+hanKanaToZenKana "ﾂﾞ" = Just 'ヅ'
+hanKanaToZenKana "ﾃﾞ" = Just 'デ'
+hanKanaToZenKana "ﾄﾞ" = Just 'ド'
+hanKanaToZenKana "ﾊﾞ" = Just 'バ'
+hanKanaToZenKana "ﾋﾞ" = Just 'ビ'
+hanKanaToZenKana "ﾌﾞ" = Just 'ブ'
+hanKanaToZenKana "ﾍﾞ" = Just 'ベ'
+hanKanaToZenKana "ﾎﾞ" = Just 'ボ'
+hanKanaToZenKana "ﾊﾟ" = Just 'パ'
+hanKanaToZenKana "ﾋﾟ" = Just 'ピ'
+hanKanaToZenKana "ﾌﾟ" = Just 'プ'
+hanKanaToZenKana "ﾍﾟ" = Just 'ペ'
+hanKanaToZenKana "ﾎﾟ" = Just 'ポ'
+hanKanaToZenKana "ｳﾞ" = Just 'ヴ'
+hanKanaToZenKana [c] = Just c
+hanKanaToZenKana _ = Nothing -- error ("syntax error:" ++ xs)
 
 hanKana :: String
 hanKana = "｡｢｣､･ｦｧｨｩｪｫｬｭｮｯｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝﾞﾟ"
